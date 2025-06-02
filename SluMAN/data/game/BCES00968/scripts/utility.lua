ccafb45ff1523612a3a6dcea1cb28ed3689395ef
DeathHound <acraig176@hotmail.com>
2025-06-02 18:35:35 +0100

    Add set coin count for sly 2 trilogy

---
+++ SluMAN/data/game/BCES00968/scripts/utility.lua
@@ -0,0 +1,8 @@
+﻿function sly_trilogy_set_coin_count(coins)
+	local addr = 0
+	-- TODO: Add Sly 1 and Sly 3
+	if gameName == "Sly 2" then
+		addr = 0x7349AC
+	end
+	Memory.WriteInt(addr, coins)
+end
